6e4cb712113846d05176316cc0aaa5e656b9495c
akochari <arnold.kochari@scilifelab.se>
2024-12-03 15:07:18 +0100

    rearrange a bit for Serve

--- ptp/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$DOWNLOAD" = "true" ]; then
-  git clone https://huggingface.co/pharmbio/ptp /app/inference/models && \
-  cd /app/inference/models && \
-  git lfs pull
-else
-  echo "noop"
-fi
-exit 0
